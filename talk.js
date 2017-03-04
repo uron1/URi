8f00ecd79a35782f519487fb947530a261aa0f8c
uron1 <perosakiuron84@gmail.com>
2017-03-04 14:05:19 +0900

    部品化

---
+++ talk.js
@@ -0,0 +1,28 @@
+
+        //ボタン押されたときの処理
+          function talk() {
+            // $("#textarea").val() テキストエリアの文字列取得
+            let text = $("#textarea").val();
+
+            for (var i = 0; i < serihu[0].length; i++) {
+              let ary = serihu[0][i].split(',');
+              let answer = serihu[1][i].split(',');
+
+
+                  for (var a = 0; a < ary.length; a++) {
+
+                    var wordcheck = ary[a];
+
+                    if (text.indexOf(wordcheck) != -1) {
+
+
+                      let rand = Math.floor( Math.random() * 2 ) ;
+                    alert(answer[rand]);
+                    //ランダム
+
+                    }
+                  }
+
+            }
+
+          }
