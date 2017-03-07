41f3c82c01a4d8092b3825a6101c1d5f205455de
chanp115 <yuya3226@yahoo.co.jp>
2017-03-07 20:37:42 +0900

    getHour()の変数宣言をletからconstに変更

--- talk.js
+++ talk.js
@@ -5,18 +5,18 @@ function talk() {
     const text = $("#textarea").val();//テキストエリアの文字列取得
 
     //特別な処理を必要とする文字列が入力された場合
-    if (text === "こんにちわ" || text === "こんにちは") {
+    if (text=== "こんにちわ" ||text=== "こんにちは") {
         const h = getHour();
         if(h >= 5 && h <= 9){
           changeText("今の時間は「おはよう」って言いましょうよ！");
-        }else if(h >= 10 && h <= 17){
+        }else if(h >=10 && h<=17){
           changeText("こんにちは！！");
         }else{
           changeText("今の時間は「こんばんわ」って挨拶しましょう！");
         }
 
         return;//出力後関数を抜ける
-  }else if(text === "こんばんわ" ||text === "こんばんは"){
+  }else if(text=== "こんばんわ" ||text=== "こんばんは"){
           const h = getHour();
           if(h >= 5 && h <= 9){
             changeText("今の時間は「おはよう」って言いましょうよ！");
@@ -26,11 +26,11 @@ function talk() {
             changeText("こんばんわ！！");
           }
         return;
-  }else if(text === "おはよう" || text === "おはよ！"){
+  }else if(text=== "おはよう" ||text=== "おはよ！"){
           const h = getHour();
           if(h >= 5 && h <= 9){
             changeText("おはよう！！");
-          }else if(h >= 10 && h <= 17){
+          }else if(h >=10 && h<=17){
             changeText("今の時間は「こんにちは」って言う挨拶が正しいですね");
           }else{
             changeText("今の時間は「こんばんわ」って言いましょう");
@@ -51,12 +51,12 @@ function talk() {
           const wordcheck = quesArray[j];
 
           if (text.indexOf(wordcheck) != -1) {//入力文字列がserif.jsにある単語だったとき
-              const rand = Math.floor(Math.random() * answerArray.length);
+              const rand = Math.floor( Math.random() * answerArray.length );
               changeText(answerArray[rand]);
               return;//出力後関数を抜ける
           }else{//入力文字列がserif.jsにない場合
             const nothingText = ["何言ってんのかわかんない・・・","違う話しよ！"];
-            const rand = Math.floor(Math.random() * nothingText.length);
+            const rand = Math.floor( Math.random() * nothingText.length );
             changeText(nothingText[rand]);
             }
         }
@@ -71,7 +71,7 @@ function changeText(outText){
 
 //日本時間のhourを0〜24の数値で取得
 function getHour(){
-  let now = new Date();
-  let h = now.getHours();
+  const now = new Date();
+  const h = now.getHours();
     return h;
 }
